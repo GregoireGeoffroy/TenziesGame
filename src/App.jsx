41eccfdd36f9b77332146cc43bfaa8ea80c3437b
GregoireGeoffroy <thegathering92@gmail.com>
2024-10-10 09:00:30 +0300

    setup end game useEffectt

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import Die from './Die'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { nanoid } from 'nanoid'
 
 export default function App() {
   const [dice, setDice] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false)
+
+  useEffect(() => {
+    console.log("Dice state changed")
+}, [dice])
     
   function generateNewDie() {
     return {
